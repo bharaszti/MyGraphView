999c5e2a3e64accad231088b273d9bc8cf217fbe
bence <bence.haraszti@gmail.com>
2015-07-14 23:05:29 +0200

    added more data, bar selection, fixed Y range

--- app/src/main/java/org/doit/meter/ui/DiagramActivity.java
+++ app/src/main/java/org/doit/meter/ui/DiagramActivity.java
@@ -5,24 +5,37 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
 
 import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.LegendRenderer;
+import com.jjoe64.graphview.ValueDependentColor;
 import com.jjoe64.graphview.helper.StaticLabelsFormatter;
 import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.DataPointInterface;
 import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.OnDataPointTapListener;
+import com.jjoe64.graphview.series.Series;
 import com.jjoe64.graphview_demos.R;
 
+import java.util.Random;
+
 public class DiagramActivity extends ActionBarActivity {
+    private static final int MAX_DATA_POINTS = 12;
+    private static final int DEFAULT_COLOR = Color.GREEN;
+    private static final int SELECTED_COLOR = Color.YELLOW;
+
+    private DataPoint data1[] = new DataPoint[MAX_DATA_POINTS];
+    private DataPoint data2[] = new DataPoint[MAX_DATA_POINTS];
+    private String labels[] = new String[MAX_DATA_POINTS];
+    private Double selectedX;
+    private GraphView graph;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diagram);
-        updateView();
+        setupView();
     }
 
     @Override
@@ -47,37 +60,71 @@ public class DiagramActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void updateView() {
-        TextView text = (TextView) this.findViewById(R.id.section_label);
-        text.setText("Diagram title");
+    private void setupView() {
+        // data
+        Random random = new Random();
+        for (int i = 0; i < MAX_DATA_POINTS; i++) {
+            int value = 100 + random.nextInt(20);
+            data1[i] = new DataPoint(i, value);
+            data2[i] = new DataPoint(i, value + 10 - random.nextInt(20));
+            labels[i] = String.valueOf(i + 1);
+        }
 
-        GraphView graph = (GraphView) this.findViewById(R.id.graph);
-        BarGraphSeries<DataPoint> series = new BarGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
-        });
+        this.graph = (GraphView) this.findViewById(R.id.graph);
+        BarGraphSeries<DataPoint> series = new BarGraphSeries<>(data1);
         graph.addSeries(series);
 
-        LineGraphSeries<DataPoint> series2 = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 3),
-                new DataPoint(1, 3),
-                new DataPoint(2, 6),
-                new DataPoint(3, 2),
-                new DataPoint(4, 5)
-        });
+        LineGraphSeries<DataPoint> series2 = new LineGraphSeries<>(data2);
         graph.addSeries(series2);
 
         // style
-        series.setColor(Color.rgb(255, 120, 120));
-        series.setSpacing(50);
+        series.setSpacing(10);
 
-        // use static labels for horizontal and vertical labels
+        // Y axis
+        graph.getViewport().setYAxisBoundsManual(true);
+        graph.getViewport().setMinY(0);
+        graph.getViewport().setMaxY(150);
+
+        // use static labels for X axis
         StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph);
-        staticLabelsFormatter.setHorizontalLabels(new String[]{"01\n2015", "02/15\n ", "03/15\n ", "04/15\n ", "05/15\n "});
+        staticLabelsFormatter.setHorizontalLabels(labels);
         graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
+
+        // selected bar styling
+        series.setValueDependentColor(new ValueDependentColor<DataPoint>() {
+            @Override
+            public int get(DataPoint data) {
+                Double selected = DiagramActivity.this.selectedX;
+                if (selected != null && selected.equals(data.getX())) {
+                    return SELECTED_COLOR;
+                }
+                return DEFAULT_COLOR;
+            }
+        });
+
+        // register tap on series callback
+        series.setOnDataPointTapListener(new OnDataPointTapListener() {
+            @Override
+            public void onTap(Series series, DataPointInterface dataPoint) {
+                DiagramActivity.this.selectDataPoint(dataPoint);
+            }
+        });
+
+        // select default bar
+        selectDataPoint(data1[MAX_DATA_POINTS - 1]);
+    }
+
+    private void selectDataPoint(DataPointInterface dataPoint) {
+        Double x = dataPoint.getX();
+        Double y = dataPoint.getY();
+
+        TextView text = (TextView) DiagramActivity.this.findViewById(R.id.section_label);
+        text.setText(String.format("Selected: x=%s, y=%d", labels[x.intValue()], y.intValue()));
+
+        this.selectedX = x;
+        if (graph != null) {
+            graph.invalidate();
+        }
     }
 
 }
